49789ec5ae34927c14f174b6da32db57c4547740
Lucas Ambrósio <38929679+amb-lucas@users.noreply.github.com>
2019-03-27 15:58:14 -0300

    Update Inv_MOD.cpp

--- Inv_MOD.cpp
+++ Inv_MOD.cpp
@@ -1,8 +1,19 @@
 
-int x, y;
-int g = extGCD(a, m, &x, &y);
+// Para apenas um determinado N
 
-if (g == 1) x = (x % m + m) % m;
+int x, y;
+int g = extGCD(N, MOD, &x, &y);
+if (g == 1) x = (x % MOD + MOD) % MOD;
 
 // Resposta em x
 // Existe se e somente si a e m forem primos entre si
+
+
+// Para todos os números entre 1 e MOD-1
+
+inv[1] = 1;
+for(int i = 2; i<MOD; i++){
+	inv[i] = (MOD - ((MOD/i)*inv[MOD%i])%MOD) %MOD;
+}
+
+// Note que os números >= a MOD são congruentes à num%mod
