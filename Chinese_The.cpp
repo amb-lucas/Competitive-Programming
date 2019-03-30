ef12628bc4ff17de5246c05b299bdbe45530278b
Lucas Ambrósio <38929679+amb-lucas@users.noreply.github.com>
2019-03-30 00:22:41 -0300

    Update Chinese_The.cpp

--- Chinese_The.cpp
+++ Chinese_The.cpp
@@ -1,35 +1,40 @@
 
+// Returns correct rest of r in mod m
+ll normalize(ll r, ll m){
+	return ((r%m)+m)%m;
+}
+
 // Parameter R for rests and MOD for modules
 // Returns the unique solution format {rest, mod}
 // Or {-1, -1} in case there is no solution
 
 pair<ll, ll> CRT(vector<ll> R, vector<ll> MOD){
-
-    ll A = R[0]%MOD[0];
+ 
+    ll A = R[0];
     ll M = MOD[0];
-
+ 
     for(int i=1; i<R.size(); i++){
 
-        ll a2 = R[i];
-        
-        ll g = gcd(M, MOD[i]);
-        if(A%g != R[i]%g) return {-1, -1};
-        // If different, there's no solution
-        
+        ll a = R[i];
+        ll m = MOD[i];
+ 
+        ll g = gcd(M, m);
+        if (A%g != a%g) return {-1,-1};
+ 
         ll p, q;
-        extGCD(M/g, MOD[i]/g, p, q);
+        extGCD(M/g, m/g, p, q);
+ 
+        ll mod = (M/g)*m;
+ 
+ 		A = (A*(m/g))%mod;
+ 		A = (A*q)%mod;
 
-        // Updating MOD to incorporate new equation
-        M = M/g * MOD[i];
+ 		ll B = (a*(M/g))%mod;
+ 		B = (B*p)%mod;
 
-        // Updating rest to incorporate new equation
-        A = (A*((MOD[i]/g)%M))%M;
-        A = (A*(q%M))%M;
-
-        ll B = ((R[i]%M)*((M/g)%M))%M;
-        B = (B*(p%M))%M;
-
-        A = (A+B)%M;
+ 		A = (A+B)%mod;
+ 		M = mod;
+ 		A = normalize(A, M);
     }
 
     return {A, M};
