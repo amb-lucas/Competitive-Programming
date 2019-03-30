e20225cd6c1d85266d9692ec5868c74518f39866
Lucas Ambrósio <38929679+amb-lucas@users.noreply.github.com>
2019-03-29 23:14:15 -0300

    Update Chinese_The.cpp

--- Chinese_The.cpp
+++ Chinese_The.cpp
@@ -3,29 +3,30 @@
 // Returns the unique solution format {rest, mod}
 // Or {-1, -1} in case there is no solution
 
-pair<int, int> CRT(vector<int> R, vector<int> MOD){
+pair<ll, ll> CRT(vector<ll> R, vector<ll> MOD){
 
-    int A = R[0]%MOD[0];
-    int M = MOD[0];
+    ll A = R[0]%MOD[0];
+    ll M = MOD[0];
 
     for(int i=1; i<R.size(); i++){
 
-        int a2 = A[i];
+        ll a2 = R[i];
+        
+        ll g = gcd(M, MOD[i]);
+        if(A%g != R[i]%g) return {-1, -1};
+        // If different, there's no solution
+        
+        ll p, q;
+        extGCD(M/g, MOD[i]/g, p, q);
 
-        int g = gcd(M, MOD[i]);
-        if(A%g != A[i]%g) return {-1, -1};
-
-        int p, q;
-        extGCD(M/g, MOD[i]/g, &p, &q);
-
-        // Updating MOD to new LCM
+        // Updating MOD to incorporate new equation
         M = M/g * MOD[i];
 
         // Updating rest to incorporate new equation
         A = (A*((MOD[i]/g)%M))%M;
         A = (A*(q%M))%M;
 
-        int B = ((A[i]%M)*((M/g)%M))%M;
+        int B = ((R[i]%M)*((M/g)%M))%M;
         B = (B*(p%M))%M;
 
         A = (A+B)%M;
