4614dc47868a6c45a3765b43e873e8d1b898e094
Lucas Ambrósio <38929679+amb-lucas@users.noreply.github.com>
2019-04-06 20:37:38 -0300

    Update LCA.cpp

--- Data-Strucures/LCA.cpp
+++ Data-Strucures/LCA.cpp
@@ -9,7 +9,7 @@ void dfs(int v, int p, int l=0){ // chamar como dfs(root, root)
 
 	dpt[v] = l;
 	par[v][0] = p;
-	for(int k=1; k<mlg; k++) par[v][k] = par[par[v][k-1]][k-1];
+	for(int k=1; k<LIMLG; k++) par[v][k] = par[par[v][k-1]][k-1];
 
 	for(int u: adj[v]){
 		if(u!=p) dfs(u, v, l+1);
@@ -21,7 +21,7 @@ int lca(int a, int b) {
 	if(dpt[b]>dpt[a]) swap(a, b);
 
 	for(int i=LIMLG-1; i>=0; i--) {
-		if(lvl[a]-(1<<i) >= lvl[b]) a=par[a][i];
+		if(dpt[a]-(1<<i) >= dpt[b]) a = par[a][i];
 	}
 
 	if(a == b) return a;
