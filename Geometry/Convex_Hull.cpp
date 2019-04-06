5ec9ef217d2f3f1b3a7ba20599a5243583486603
Lucas Ambrósio <38929679+amb-lucas@users.noreply.github.com>
2019-04-06 00:30:11 -0300

    Update Convex_Hull.cpp

--- Geometry/Convex_Hull.cpp
+++ Geometry/Convex_Hull.cpp
@@ -1,6 +1,5 @@
-
 struct pt {
-    double x, y;
+    ll x, y;
 };
 
 bool cmp(pt a, pt b) {
@@ -15,38 +14,39 @@ bool ccw(pt a, pt b, pt c) {
     return a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y) > 0;
 }
 
-vector<pt> up, down;
-
 void convex_hull(vector<pt>& a) {
 
+    if(a.size() < 2) return;
+
     sort(a.begin(), a.end(), cmp);
+    vector<pt> up, down;
+    up.push_back(a[0]);
+    down.push_back(a[0]);
 
-    if(a.size()){
-        up.push_back(a[0]);
-        down.push_back(a[0]);
-    }
+    int j=1;
+    while(j<a.size() && a[0].x == a[j].x && a[0].y == a[j].y) j++;
 
-    if(a.size()>1){
-        up.push_back(a[1]);
-        down.push_back(a[1]);
-    }    
+    if(j<a.size()){
 
-    for (int i=2; i<a.size(); i++) {
+        up.push_back(a[j]);
+        down.push_back(a[j]);
 
-        while(up.size()>=2 && !cw(up[up.size()-2], up[up.size()-1], a[i]) ){
-            up.pop_back();
+        for(int i=j+1; i<a.size(); i++){
+
+            while(up.size()>=2 && !cw(up[up.size()-2], up[up.size()-1], a[i]))
+                up.pop_back();
+
+            while(down.size()>=2 && !ccw(down[down.size()-2], down[down.size()-1], a[i]))
+                down.pop_back();
+
+            up.push_back(a[i]);
+            down.push_back(a[i]);
         }
-
-        up.push_back(a[i]);
-        
-        while(down.size()>=2 && !ccw(down[down.size()-2], down[down.size()-1], a[i]) ){
-            down.pop_back();
-        }
-
-        down.push_back(a[i]);
     }
 
     a.clear();
     for(int i=0; i<up.size(); i++) a.push_back(up[i]);
-    for(int i=down.size()-2; i; i--) a.push_back(down[i]);
+    if(down.size()>2){
+        for(int i=down.size()-2; i; i--) a.push_back(down[i]);
+    }
 }
