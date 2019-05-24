0ac3a277425931a6f5764418599573de66133751
Lucas Ambrósio <38929679+amb-lucas@users.noreply.github.com>
2019-05-23 23:54:58 -0300

    Update Rabin-Karp.cpp

--- Strings/Rabin-Karp.cpp
+++ Strings/Rabin-Karp.cpp
@@ -1,4 +1,5 @@
 const int ALFSZ = 26;
+const int FAT = ALFSZ+1;
 
 ll add_left(ll act, char a, int sz){
     ll inc = a-'a'+1;
@@ -7,7 +8,7 @@ ll add_left(ll act, char a, int sz){
 }
 
 ll add_right(ll act, char a){
-    act = (act*ALFSZ)%MOD;
+    act = (act*FAT)%MOD;
     act += a-'a'+1;
     return act%MOD;
 }
