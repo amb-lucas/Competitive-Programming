83fecbef9be59611e8517cc6b7461daea95bf4a0
Lucas Ambrósio <38929679+amb-lucas@users.noreply.github.com>
2019-05-30 03:23:56 -0300

    Update Convex-Hull.cpp

--- Geometry/Convex-Hull.cpp
+++ Geometry/Convex-Hull.cpp
@@ -1,24 +1,24 @@
 
 void convex_hull(vector<pt> &v) {
 
-    sort(v.begin(), v.end());
-    v.erase(unique(v.begin(), v.end()), v.end());
+	sort(v.begin(), v.end());
+	v.erase(unique(v.begin(), v.end()), v.end());
 
-    vector<pt> up, down;
+	vector<pt> up, down;
 
-    for(pt p: v){
+	for(pt p: v){
 
-        while(up.size()>=2 && !cw(up[up.size()-2], up.back(), p) )
-            up.pop_back();
+		while(up.size()>=2 && !p.cw(up[up.size()-2], up.back()))
+			up.pop_back();
 
-        while(down.size()>=2 && !ccw(down[down.size()-2], down.back(), p) )
-            down.pop_back();
+		while(down.size()>=2 && !p.ccw(down[down.size()-2], down.back()))
+			down.pop_back();
 
-        up.push_back(p);
-        down.push_back(p);
-    }
+		up.push_back(p);
+		down.push_back(p);
+	}
 
-    v.clear();
-    v = up;
-    for(int i=down.size()-2; i>0; i--) v.push_back(down[i]);
+	v.clear();
+	v = up;
+	for(int i=down.size()-2; i>0; i--) v.push_back(down[i]);
 }
