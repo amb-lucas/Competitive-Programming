c0125eac4d75a9e62a2528321930b39f020e979a
Lucas Ambrósio <38929679+amb-lucas@users.noreply.github.com>
2019-06-04 10:44:59 -0300

    Create Polygons.cpp

---
+++ Geometry/Polygons.cpp
@@ -0,0 +1,45 @@
+
+// Determina se o ponto p esta dentro do triangulo (a, b, c)
+bool pointInsideTriangle(PT &p, PT &a, PT &b, PT &c){
+	if(pointInSegment(a,b,p)) return 1;
+	if(pointInSegment(b,c,p)) return 1;
+	if(pointInSegment(c,a,p)) return 1;
+	bool A = p.cw(a, b), B = p.cw(b, c), C = p.cw(c, a);
+	return A == B && A == C;
+}
+
+// Determina se o ponto esta num poligono convexo em O(lgn)
+bool pointInConvexPolygon(vector<PT> &p, PT &q){
+
+	if(p.size() == 0) return 0;
+	if(p.size() == 1) return p[0] == q;
+	if(p.size() == 2) return pointInSegment(p[0], p[1], q);
+
+	if(pointInSegment(p[0], p[1], q)) return 1;
+	if(q.ccw(p[0], p[1])) return 0;
+	if(q.cw(p[0], p.back())) return 0;
+
+	int l = 1, m, r = p.size()-1;
+	while(l<r-1){
+		m = (l+r)>>1;
+		if(!q.cw(p[0], p[m])) r = m;
+		else l = m;
+	}
+	return pointInsideTriangle(q, p[0], p[l], p[r]);
+}
+
+// Calcula centro do circulo dado tres pontos
+PT computeCircleCenter(PT a, PT b, PT c){
+	b = (a+b)/2;
+	c = (a+c)/2;
+	return computeLineIntersection(b, b+(a-b).rotateCW(), c, c+(a-c).rotateCW());
+}
+
+double area(vector<PT> &P){
+	double res = 0;
+	for(int i=0; i<P.size(); i++){
+		PT p = P[i], q = P[(i+1)%P.size()];
+		res += (p.x-q.x)*(p.y+q.y);
+	}
+	return dabs(res)/2;
+}
