c3c5291fd60ea109290a6fe6212169aaacde6996
Lucas Ambrósio <38929679+amb-lucas@users.noreply.github.com>
2019-06-05 14:05:33 -0300

    Update Polygons.cpp

--- Geometry/Polygons.cpp
+++ Geometry/Polygons.cpp
@@ -47,12 +47,19 @@ double area(vector<PT> &P){
 
 // Faz a interseção de um poligono com um semiplano (corte CW)
 vector<PT> cutPolygon(vector<PT> &Q, PT a, PT b){
+	
+	PT normal = (b-a).rotateCW().normalize();
 	vector<PT> resp;
-	for(int i=0, j; i<Q.size(); i++){
-		j = (i+1)%Q.size();
-		if(!Q[i].ccw(a, b)) resp.push_back(Q[i]);
-		if(Q[i].cw(a,b) != Q[j].cw(a,b))
-			resp.push_back(computeLineIntersection(a,b,Q[i],Q[j]));
+	for(int i=0; i<Q.size(); i++){
+
+		int j = (i+1)%Q.size();
+		double n1 = (Q[i]-a)*(normal);
+		double n2 = (Q[j]-a)*(normal);
+
+		if(n1>=-eps) resp.push_back(Q[i]);
+		if((n1<-eps && n2>eps) || (n1>eps && n2<-eps)){
+			resp.push_back(computeLineIntersection(a, b, Q[i], Q[j]));
+		}
 	}
 	return resp;
 }
