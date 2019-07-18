4260246dc9bbaaef849fbd57995ca48c4693525e
Lucas Ambrósio <38929679+amb-lucas@users.noreply.github.com>
2019-07-18 17:33:00 -0300

    Create Tree.cpp

---
+++ Data-Strucures/Tree.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+	tree_order_statistics_node_update> ordered_set;
+	
+// Inserir
+s.insert();
+
+// Remover
+s.erase();
+
+// Contar quantia de elementos estritamente menores que o parâmetro
+s.order_of_key();
