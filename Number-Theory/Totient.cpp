3958b39ddf32b67420e50db9a379302847372809
Lucas Ambrósio <38929679+amb-lucas@users.noreply.github.com>
2021-05-07 02:15:56 -0300

    fix totient function

--- Number-Theory/Totient.cpp
+++ Number-Theory/Totient.cpp
@@ -10,13 +10,13 @@ ll phi(ll n) {
 	for(int i = 2; i <= n/i; i++) {
 		if(n % i == 0) {
 			ans /= i;
-			ans *= (i+1);
+			ans *= (i-1);
 			while(n % i == 0) n /= i;
 		}
 	}
 	if(n > 1){
 		ans /= n;
-		ans *= (n+1);
+		ans *= (n-1);
 	}
 	return ans;
 }
